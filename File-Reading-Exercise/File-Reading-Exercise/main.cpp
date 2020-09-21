82a9b9531a527f2da5eef5c92c39bfbd010eae82
tiiedye <tiiedye@gmail.com>
2020-09-21 13:19:29 -0700

    creating print_header function

--- File-Reading-Exercise/File-Reading-Exercise/main.cpp
+++ File-Reading-Exercise/File-Reading-Exercise/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
-using namespace std;
+// Prototypes
+void print_header();
+void print_footer(double average);
+void print_student(const std::string& student, int score);
+int process_response(const std::string& response, const std::string& answer_key);
+
+// Function Definitions
+void print_header() {
+    std::cout << std::setw(15) << std::left << "Student"
+        << std::setw(15) << std::right << "Score" << std::endl;
+    std::cout << std::setw(30) << std::setfill('-') << "" << std::endl;
+    std::cout << std::setfill(' ');
+}
+
+
 
 int main() {
+    std::ifstream in_file;
+    std::string answer_key{};
+    std::string name{};
+    std::string response{};
+    int running_sum{ 0 };
+    int total_students{ 0 };
+    double average_score{ 0.0 };
 
-    cout << "Hello world" << endl;
+    print_header();
+
     return 0;
 }
