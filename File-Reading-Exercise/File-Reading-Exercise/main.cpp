75116dd098fe6db38b80eb1bfec7f1edd8bdc790
tiiedye <tiiedye@gmail.com>
2020-09-21 13:38:06 -0700

    finishing up writing the main program

--- File-Reading-Exercise/File-Reading-Exercise/main.cpp
+++ File-Reading-Exercise/File-Reading-Exercise/main.cpp
@@ -12,6 +12,7 @@ int process_response(const std::string& response, const std::string& answer_key)
 void print_header() {
     std::cout << std::setw(15) << std::left << "Student"
         << std::setw(15) << std::right << "Score" << std::endl;
+
     std::cout << std::setw(30) << std::setfill('-') << "" << std::endl;
     std::cout << std::setfill(' ');
 }
@@ -52,7 +53,32 @@ int main() {
     int total_students{ 0 };
     double average_score{ 0.0 };
 
+    in_file.open("../response.txt");
+
+    if (!in_file) {
+        std::cerr << "There was an error opening a file" << std::endl;
+        return 1;
+    }
+
+    in_file >> answer_key;
+
     print_header();
+
+    while (in_file >> name >> response) {
+        ++total_students;
+        int score = process_response(response, answer_key);
+        running_sum += score;
+        print_student(name, score);
+    }
+
+    if (total_students != 0) {
+        average_score = static_cast<double>(running_sum) / total_students;
+    }
+
+    print_footer(average_score);
+
+    in_file.close();
+    std::cout << std::endl << std::endl;
 
     return 0;
 }
